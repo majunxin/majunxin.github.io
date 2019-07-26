90d36b1f1bdc2e2cee8a90c13b2416bc1aa4c9e5
majunxin <343449152@qq.com>
2019-07-26 19:11:58 +0800

    Signed-off-by: majunxin <343449152@qq.com>

--- Dice/web-desktop/main.bf4b8.js
+++ Dice/web-desktop/main.bf4b8.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.974d3.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.25f6f.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.e0b5b.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.bf4b8.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
