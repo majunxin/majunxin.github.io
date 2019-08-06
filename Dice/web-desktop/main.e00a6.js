c5eecae2e6d73324d390f8512419835351db04e0
majunxin <343449152@qq.com>
2019-08-06 18:22:17 +0800

    Signed-off-by: majunxin <343449152@qq.com>

--- Dice/web-desktop/main.e00a6.js
+++ Dice/web-desktop/main.e00a6.js
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.19c6f.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.e00a6.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -201,12 +201,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.5caf8.js');
+        require('src/settings.d807d.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.5caf8.js');
+        require('src/settings.d807d.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
