06b40d7383ed0e4f4ebad134ac5c54a8b374a720
majunxin <343449152@qq.com>
2019-08-08 10:42:49 +0800

    Signed-off-by: majunxin <343449152@qq.com>

--- Dice/web-mobile/main.b7db6.js
+++ Dice/web-mobile/main.b7db6.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.5c4f4.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.769a8.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.e051e.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.b7db6.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -201,12 +201,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.f5867.js');
+        require('src/settings.3b933.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.f5867.js');
+        require('src/settings.3b933.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
