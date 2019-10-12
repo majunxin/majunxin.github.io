dd30d545a78f97f33f3489320767de3c35d151ad
majunxin <343449152@qq.com>
2019-10-12 15:40:07 +0800

    Signed-off-by: majunxin <343449152@qq.com>

--- Dice/web-desktop/main.85387.js
+++ Dice/web-desktop/main.85387.js
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.6bdec.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.85387.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -201,12 +201,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.d504c.js');
+        require('src/settings.a5cf9.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.d504c.js');
+        require('src/settings.a5cf9.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
